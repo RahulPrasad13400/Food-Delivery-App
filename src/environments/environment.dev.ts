ad7ce0a349f23b2cf0836faf4c862baf2836d4db
Rahul Prasad <rahulprasad1399@gmail.com>
2025-05-28 23:14:11 +0530

    Delete environment.dev.ts

--- src/environments/environment.dev.ts
+++
@@ -1,6 +0,0 @@
-import { Environment } from "./environment";
-
-export const DevEnvironment : Environment = { 
-    // db_uri : 'mongodb+srv://marsupilamigaming8:V3MI2995LfjimeiA@cluster0.jqzxtzm.mongodb.net/foodDeliveryApp?retryWrites=true&w=majority&appName=Cluster0',
-    // sendgrid_api_key : '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'
-}
